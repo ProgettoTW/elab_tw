5c3ae22fdd38c07c937808fbd25c5d36963a4805
Luca Bandini <luca.vombato@gmail.com>
2022-01-12 17:30:43 +0100

    Code Cleanup

--- includes/header.php
+++ includes/header.php
@@ -41,7 +41,7 @@ if (isset($_SESSION['user_id'], $_SESSION['cart_id'])) {
         position: absolute;
         background-color: #f1f1f1;
         min-width: 160px;
-        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
         z-index: 1;
     }
 
@@ -54,13 +54,19 @@ if (isset($_SESSION['user_id'], $_SESSION['cart_id'])) {
     }
 
     /* Change color of dropdown links on hover */
-    .dropdown-content a:hover {background-color: #ddd;}
+    .dropdown-content a:hover {
+        background-color: #ddd;
+    }
 
     /* Show the dropdown menu on hover */
-    .dropdown:hover .dropdown-content {display: block;}
+    .dropdown:hover .dropdown-content {
+        display: block;
+    }
 
     /* Change the background color of the dropdown button when the dropdown content is shown */
-    .dropdown:hover .dropbtn {background-color: #3e8e41;}
+    .dropdown:hover .dropbtn {
+        background-color: #3e8e41;
+    }
 </style>
 
 <!DOCTYPE html>
@@ -70,7 +76,8 @@ if (isset($_SESSION['user_id'], $_SESSION['cart_id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- CSS -->
     <link rel="stylesheet" href="css/style.css">
     <!--Google Font-->
@@ -86,7 +93,8 @@ if (isset($_SESSION['user_id'], $_SESSION['cart_id'])) {
     <nav class="navbar navbar-expand-lg text-uppercase navbar-dark">
         <a class="navbar-brand" href="#">
             <h1 class="h1">Pastecceroo</h1></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-lg-between" id="navbarSupportedContent">
@@ -109,11 +117,13 @@ if (isset($_SESSION['user_id'], $_SESSION['cart_id'])) {
                     </form>
                 </li>
             </ul>
-            <?php if (login_check($db)){ ?>
-                <div class="dropdown"><?php echo $_SESSION['nome']; ?> - <b><?php if (admin_check($db)) {echo "ADMIN";} ?>
+            <?php if (login_check($db)) { ?>
+                <div class="dropdown"><?php echo $_SESSION['nome']; ?> - <b><?php if (admin_check($db)) {
+                            echo "ADMIN";
+                        } ?>
                         <div class="dropbtn">Menu</div>
                         <div class="dropdown-content">
-                            <?php if (admin_check($db)){ ?>
+                            <?php if (admin_check($db)) { ?>
                                 <a href="#">Pagina amministratore</a>
                             <?php } ?>
                             <a href="#">Il mio account</a>
@@ -124,14 +134,18 @@ if (isset($_SESSION['user_id'], $_SESSION['cart_id'])) {
                 </div>
                 <?php
             } else { ?>
-            <ul class="navbar-nav auth">
-                <li class="nav-item">
-                    <a href="login_page.php"><button type="button" class="btn btn-primary">Accedi</button></a>
-                </li>
-                <li class="nav-item">
-                    <a href="register_page.php"><button type="button" class="btn btn-secondary">Registrati</button></a>
-                </li>
-            </ul>
+                <ul class="navbar-nav auth">
+                    <li class="nav-item">
+                        <a href="login_page.php">
+                            <button type="button" class="btn btn-primary">Accedi</button>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="register_page.php">
+                            <button type="button" class="btn btn-secondary">Registrati</button>
+                        </a>
+                    </li>
+                </ul>
                 <?php
             } ?>
         </div>
