beb5e4b401a8b3e7da95b80c791b6192f3d1c1f8
Luca Bandini <luca.vombato@gmail.com>
2022-01-12 16:48:40 +0100

    Moved index to main and added session check

--- index.php
+++ index.php
@@ -23,234 +23,99 @@
      <title>Pastecceroo</title>
  </head>
 
- <body class="bg-light">
+ <body class="body">
  <!-- Container -->
- <div class="container-fluid">
-     <!-- Div H1 -->
-     <div class="row">
-         <div class="col-12">
-             <header class=" header text-center text-white"><h1>Pastecceroo</h1></header>
+
+ <div class="py-5 align-items-center d-flex">
+     <div class="container py-5">
+         <div class="row">
+             <div class="col-md-6 px-md-5">
+                 <h1 class="display-3 mb-4">Giornata Pesante?</h1>
+                 <p class="lead mb-4">Rilassati con qualche dolce!<br></p> <a href="#" class="btn btn-lg btn-primary">Vai allo Shop!</a>
+             </div>
+             <div class="col-md-6"><img class="img-fluid d-block" src="img/img1.jpg"></div>
          </div>
      </div>
-     <!-- Div NavBar -->
-     <div class="row">
-         <div class="col-12">
-             <nav class="navbar navbar-expand-lg navbar-light bg-light text-uppercase fw-bold nav">
-                 <!-- Div NavBar <a class="navbar-brand" href="#">
-                    <img src="./logo1.png" alt="" width="150"></a>-->
-                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                     <span class="navbar-toggler-icon"></span>
-                 </button>
-                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                     <ul class="navbar-nav me-auto mx-auto">
-                         <li class="nav-item">
-                             <a class="nav-link active" aria-current="page" href="#">Home</a>
-                         </li>
-                         <li class="nav-item">
-                             <a class="nav-link active" href="#">Prodotti</a>
-                         </li>
-                         <li class="nav-item dropdown">
-                             <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                 Categorie
-                             </a>
-                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                 <li><a class="dropdown-item" href="#">Torte</a></li>
-                                 <li><a class="dropdown-item" href="#">Cupcakes</a></li>
-                                 <li><a class="dropdown-item" href="#">Biscotti</a></li>
-                                 <li><a class="dropdown-item" href="#">Brownie</a></li>
-                             </ul>
-                         </li>
-                         <li class="nav-item">
-                             <a class="nav-link active" href="#">Contatti</a>
-                         </li>
-                     </ul>
-                     <ul class="navbar-nav me-auto mx-auto col-1">
-                         <li class="nav-item">
-                             <button type="button" class="btn btn-primary px-2">Accedi</button>
-                         </li>
-                         <li class="nav-item">
-                             <button type="button" class="btn btn-secondary px-2">Registrati</button>
-                         </li>
-                 </div>
-             </nav>
-             <hr>
-         </div>
-     </div>
-     <main>
-         <div class="d-flex justify-content-lg-around flex-wrap">
-             <div class="card">
-                 <img src="img/biscotti.jpg" class="card-img-top" alt="">
-                 <div class="card-body">
-                     <h5 class="card-title">Card title</h5>
-                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                     <a href="#" class="btn btn-primary">Go somewhere</a>
-                 </div>
-             </div>
-             <div class="card">
-                 <img src="img/biscotti.jpg" class="card-img-top" alt="">
-                 <div class="card-body">
-                     <h5 class="card-title">Card title</h5>
-                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                     <a href="#" class="btn btn-primary">Go somewhere</a>
-                 </div>
-             </div>
-             <div class="card">
-                 <img src="img/biscotti.jpg" class="card-img-top" alt="">
-                 <div class="card-body">
-                     <h5 class="card-title">Card title</h5>
-                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                     <a href="#" class="btn btn-primary">Go somewhere</a>
-                 </div>
-             </div>
-             <div class="card">
-                 <img src="img/biscotti.jpg" class="card-img-top" alt="">
-                 <div class="card-body">
-                     <h5 class="card-title">Card title</h5>
-                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                     <a href="#" class="btn btn-primary">Go somewhere</a>
-                 </div>
-             </div>
-             <div class="card">
-                 <img src="img/biscotti.jpg" class="card-img-top" alt="">
-                 <div class="card-body">
-                     <h5 class="card-title">Card title</h5>
-                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                     <a href="#" class="btn btn-primary">Go somewhere</a>
-                 </div>
-             </div>
-         </div>
-     </main>
-     <footer>
-         <!-- Footer -->
-         <footer class="text-center text-lg-start bg-dark text-muted">
-             <!-- Section: Social media -->
-             <section
-                     class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
-             >
-                 <!-- Left -->
-                 <div class="me-5 d-none d-lg-block">
-                     <span>Get connected with us on social networks:</span>
-                 </div>
-                 <!-- Left -->
-
-                 <!-- Right -->
-                 <div>
-                     <a href="" class="me-4 text-reset">
-                         <i class="fab fa-facebook-f"></i>
-                     </a>
-                     <a href="" class="me-4 text-reset">
-                         <i class="fab fa-twitter"></i>
-                     </a>
-                     <a href="" class="me-4 text-reset">
-                         <i class="fab fa-google"></i>
-                     </a>
-                     <a href="" class="me-4 text-reset">
-                         <i class="fab fa-instagram"></i>
-                     </a>
-                     <a href="" class="me-4 text-reset">
-                         <i class="fab fa-linkedin"></i>
-                     </a>
-                     <a href="" class="me-4 text-reset">
-                         <i class="fab fa-github"></i>
-                     </a>
-                 </div>
-                 <!-- Right -->
-             </section>
-             <!-- Section: Social media -->
-
-             <!-- Section: Links  -->
-             <section class="">
-                 <div class="container text-center text-md-start mt-5">
-                     <!-- Grid row -->
-                     <div class="row mt-3">
-                         <!-- Grid column -->
-                         <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                             <!-- Content -->
-                             <h6 class="text-uppercase fw-bold mb-4">
-                                 <i class="fas fa-gem me-3"></i>Company name
-                             </h6>
-                             <p>
-                                 Here you can use rows and columns to organize your footer content. Lorem ipsum
-                                 dolor sit amet, consectetur adipisicing elit.
-                             </p>
-                         </div>
-                         <!-- Grid column -->
-
-                         <!-- Grid column -->
-                         <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                             <!-- Links -->
-                             <h6 class="text-uppercase fw-bold mb-4">
-                                 Products
-                             </h6>
-                             <p>
-                                 <a href="#!" class="text-reset">Angular</a>
-                             </p>
-                             <p>
-                                 <a href="#!" class="text-reset">React</a>
-                             </p>
-                             <p>
-                                 <a href="#!" class="text-reset">Vue</a>
-                             </p>
-                             <p>
-                                 <a href="#!" class="text-reset">Laravel</a>
-                             </p>
-                         </div>
-                         <!-- Grid column -->
-
-                         <!-- Grid column -->
-                         <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                             <!-- Links -->
-                             <h6 class="text-uppercase fw-bold mb-4">
-                                 Useful links
-                             </h6>
-                             <p>
-                                 <a href="#!" class="text-reset">Pricing</a>
-                             </p>
-                             <p>
-                                 <a href="#!" class="text-reset">Settings</a>
-                             </p>
-                             <p>
-                                 <a href="#!" class="text-reset">Orders</a>
-                             </p>
-                             <p>
-                                 <a href="#!" class="text-reset">Help</a>
-                             </p>
-                         </div>
-                         <!-- Grid column -->
-
-                         <!-- Grid column -->
-                         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                             <!-- Links -->
-                             <h6 class="text-uppercase fw-bold mb-4">
-                                 Contact
-                             </h6>
-                             <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
-                             <p>
-                                 <i class="fas fa-envelope me-3"></i>
-                                 info@example.com
-                             </p>
-                             <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
-                             <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
-                         </div>
-                         <!-- Grid column -->
-                     </div>
-                     <!-- Grid row -->
-                 </div>
-             </section>
-             <!-- Section: Links  -->
-
-             <!-- Copyright -->
-             <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-                 © 2021 Copyright:
-                 <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-             </div>
-             <!-- Copyright -->
-         </footer>
-         <!-- Footer -->
-     </footer>
  </div>
-
+ <div class="text-center py-5">
+     <div class="container">
+         <div class="row">
+             <div class="col-md-12">
+                 <h1 class="mb-3">Gusta i tuoi dolci direttamente al Campus!</h1>
+             </div>
+         </div>
+         <div class="row">
+             <div class="col-12"> <a href="#" class="text-muted">
+                     <i class="fa fa-fw fa-facebook fa-2x mx-3"></i>
+                 </a> <a href="#" class="text-muted">
+                     <i class="fa fa-fw fa-twitter fa-2x mx-3"></i>
+                 </a> <a href="#" class="text-muted">
+                     <i class="fa fa-fw fa-2x fa-google-plus mx-3"></i>
+                 </a> <a href="#" class="text-muted">
+                     <i class="fa fa-fw fa-instagram fa-2x mx-3"></i>
+                 </a> </div>
+         </div>
+     </div>
+ </div>
+ <div class="py-5">
+     <div class="container">
+         <div class="row">
+             <div class="col-md-6"><img class="img-fluid d-block" src="img/img2.jpg"></div>
+             <div class="col-md-6">
+                 <div class="px-md-5 p-3 col-md-12 d-flex flex-column align-items-start justify-content-center">
+                     <h1>Lorem Ipsum</h1>
+                     <p class="mb-3 lead">Boh non so che scrivere qui... Aggiungiamo qualche descrizione? Qualche aiuto per scegliere i dolci?</p>
+                     <div class="row">
+                         <div class="col-md-12"> <i class="fa fa-stop-circle fa-3x mr-3 text-muted d-inline"></i> <i class="fa fa-circle-o fa-3x mx-3 text-muted d-inline"></i> <i class="fa fa-stop-circle-o fa-3x mx-3 text-muted d-inline"></i> <i class="fa fa-circle fa-3x ml-3 text-muted d-inline"></i> </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+ <main>
+     <div class="first-image d-flex justify-content-center">
+     </div>
+     <div class="text d-flex flex-column text-center">
+     </div>
+ </main>
+ <footer class="text-center text-lg-start bg-dark text-muted">
+     <section class="">
+         <div class="container text-center text-md-start mt-5">
+             <div class="row mt-3">
+                 <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                     <h3 class="text-uppercase fw-bold my-4">Pastecceroo</h3>
+                     <p> Compagnia di consegna di dolci per studenti e docenti presso l'Alma Mater Studiorum - Università di Bologna - Campus di Cesena <br> <br> Scopri centinaia di prodotti artigianali prodotti dalle migliori Pasticcerie della zona. <br> <br> Inserisci la data di nascita corretta, al tuo compleanno riceverai una torta da gustare con chi vuoi! </p>
+                 </div>
+                 <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                     <h3 class="text-uppercase fw-bold my-4"> Prodotti </h3>
+                     <p>
+                         <a href="#!" class="text-reset">Torte</a>
+                     </p>
+                     <p>
+                         <a href="#!" class="text-reset">Biscotti</a>
+                     </p>
+                     <p>
+                         <a href="#!" class="text-reset">Capcakes</a>
+                     </p>
+                     <p>
+                         <a href="#!" class="text-reset">Brownie</a>
+                     </p>
+                 </div>
+                 <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                     <h3 class="text-uppercase fw-bold my-4">Contatti</h3>
+                     <p>Via Cesare Pavese, 50, 47521 Cesena FC</p>
+                     <p>infoPastecceroo@example.com</p>
+                 </div>
+             </div>
+         </div>
+     </section>
+     <div class="copyright text-center p-4"> © 2021 Copyright: <a class="text-reset fw-bold" href="index.html">Pastecceroo.it</a>
+     </div>
+ </footer>
  <!-- Option 1: Bootstrap Bundle with Popper -->
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" style=""></script>
  </body>
+
+ </html>
  </html>
