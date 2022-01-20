d492e7cfdf78ca44f34c0efa21d94eb3e95ee287
Luca Bandini <luca.vombato@gmail.com>
2022-01-20 02:59:11 +0100

    Remaining Cart functions

--- utenteCarrello.php
+++ utenteCarrello.php
@@ -1,11 +1,56 @@
 <?php
 
 require_once("includes/header.php");
+require_once("includes/session.php");
+require_once("includes/connection.php");
+require_once("cart_manager.php");
+require_once("categories.php");
 require_once("products.php");
+require_once("model/product.php");
 require_once("model/cart.php");
 require_once("model/cart_item.php");
+//altri require
 
 
+if (isset($_SESSION['user_id'], $_SESSION['cart_id'])) {
+    $userId = $_SESSION['user_id'];
+    $cartId = $_SESSION['cart_id'];
+} else  {
+//TODO REDIRECT SE SESSIONE NON È CREATA
+}
+
+$cartmanager = new CartManager();
+$products = new ProductDB();
+
+if(isset($_POST['add'])){
+    $productId = intval($_POST['add']);
+    if ($cartmanager->isInCart($productId, $userId)){
+        $row = $cartmanager->getByProductId($productId, $userId);
+        $quantity = $row[0]->getQuantity()+1;
+        $cartmanager->updateQuantity($row[0]->getId(), $quantity);
+    } else {
+        $item = new Cart_item($productId, 1, $cartId);
+        $cartmanager->insertItem($item);
+    }
+} else if(isset($_POST['remove'])) {
+    $productId = intval($_POST['remove']);
+    if ($cartmanager->isInCart($productId, $userId)){
+        $row = $cartmanager->getByProductId($productId, $userId);
+        $quantity = $row[0]->getQuantity()+1;
+        $cartmanager->updateQuantity($row[0]->getId(), 0);
+    } else {
+        echo ("Il prodotto non è nel carrello");
+    }
+} else if (isset($_POST['quantity'], $_POST['product_id'])){
+    $productId = $_POST['product_id'];
+    if ($cartmanager->isInCart($productId, $userId)){
+        $row = $cartmanager->getByProductId($productId, $userId);
+        $quantity = $_POST['quantity'];
+        $cartmanager->updateQuantity($row[0]->getId(), $quantity);
+    }
+} else if (isset($_POST['empty'])){
+    $cartmanager->emptyCart($userId);
+}
 ?>
 <html lang="it">
         <?php
@@ -26,6 +71,9 @@ require_once("model/cart_item.php");
                     <h6 class="my-0">Torta alla fragola</h6>
                     <small class="text-muted">Quantità: 1</small>
                   </div>
+                    <div>
+                        <button class="btn w-20 btn-group-sm btn-danger btn-sm">  -  </button>
+                    </div>
                   <span class="text-muted">€ 12</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between lh-sm">
